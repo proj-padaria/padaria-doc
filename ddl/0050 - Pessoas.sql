8aca6d1ea1749e5ce2fed8ff21db5b2692f19c9a
Renan Meira <meirarenan@hotmail.com>
2023-08-14 22:47:23 -0300

    adicionado clientes e fornecedores

--- ddl/0050 - Pessoas.sql
+++ ddl/0050 - Pessoas.sql
@@ -27,3 +27,30 @@ CREATE TABLE pessoas (
 
 CREATE INDEX cidade_id_idx ON pessoas(cidade_id);
 CREATE INDEX x_pessoas_nome ON pessoas(nome);
+
+CREATE TABLE fornecedores (
+  pessoa_id INT NOT NULL PRIMARY KEY
+						 REFERENCES pessoas (id)
+    						ON DELETE RESTRICT
+    						ON UPDATE CASCADE,
+  cnpj VARCHAR(15) NOT NULL
+);
+
+CREATE INDEX fk_fornecedor_pessoa_id ON fornecedores (pessoa_id);
+CREATE INDEX x_fornecedores_cnpj ON fornecedores (cnpj);
+
+CREATE TABLE IF NOT EXISTS clientes (
+  pessoa_id INT NOT NULL PRIMARY KEY
+						 REFERENCES pessoas (id)
+    						ON DELETE RESTRICT
+    						ON UPDATE CASCADE,
+  data_nascimento DATE NULL,
+  renda_mensal DECIMAL(14,2) NOT NULL DEFAULT 0,
+  documento VARCHAR(20) NULL,
+  limite_de_credito DECIMAL(14,2) NOT NULL DEFAULT 0,
+  dia_vencimento_fiado INT NOT NULL DEFAULT 10
+);
+
+CREATE INDEX pessoas_id_idx ON clientes (pessoa_id);
+
+
