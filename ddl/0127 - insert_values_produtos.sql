2652cce427c70df075907d45f0f60ffd0605b9bc
Gustavo <meiragustavo@hotmail.com>
2023-08-21 20:10:16 -0300

    Deletado por nome incorreto

--- ddl/0127 - insert_values_produtos.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO produtos (departamento_id,nome,nome_abreviado,preco_venda,quantidade_em_estoque,codigo_barra,ponto_pedido)					
-
-VALUES 
-(7,'Abacaxi pineapple','Abacaxi',7.90,10,'ABX001',5)
-(7,'Beterraba beet','Beterraba',2.50,10,'BTR001',5)
-
