a6fd26a83899d92edd89acfddab5db46a1c3d97e
Gustavo <meiragustavo@hotmail.com>
2023-09-20 22:19:03 -0300

    Ajustado o valor do ID para 1 , para evitar codigo do diversos

--- ddl/0263 - trigger_procura_codigo_barra_vago.sql
+++ ddl/0263 - trigger_procura_codigo_barra_vago.sql
@@ -5,24 +5,23 @@ DECLARE
 	r record;
 	produto_id INTEGER;
 BEGIN
-	produto_id = 0;
+	produto_id = 1;
 	
 	FOR r IN(SELECT ID 
 			  FROM produtos
-			  WHERE ID < 10000
+			  WHERE ID < 10000 AND 
+			 		ID > 1
 			  ORDER BY ID)
 	LOOP
 		produto_id = produto_id + 1;
-	IF (produto_id <> r.id) THEN
+		IF (produto_id <> r.id) THEN
 			RETURN produto_id;
-	ELSEIF (produto_id > 9999)THEN
-	RAISE EXCEPTION 'Todos os valores vagos em codigo_barra foram preenchidos favor excluir codigos sem utilização';
+		ELSEIF (produto_id > 9999)THEN
+			RAISE EXCEPTION 'Todos os valores vagos em codigo_barra foram preenchidos favor excluir codigos sem utilização';
 			EXIT;
-	END IF;
+		END IF;
 	END LOOP;
 END;		
 $$ LANGUAGE plpgsql;
-
-select f_procura_produto_balanca_vago();
-					
+			
 UPDATE empresa SET db_versao = 'B0263';
