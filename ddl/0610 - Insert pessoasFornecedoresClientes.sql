5b974c145d2b2e0385ce9602d99372e33efec432
Gustavo <meiragustavo@hotmail.com>
2023-09-22 20:45:05 -0300

    Ajustado ordem dos delete para rodar ddl sem erros

--- ddl/0610 - Insert pessoasFornecedoresClientes.sql
+++ ddl/0610 - Insert pessoasFornecedoresClientes.sql
@@ -1,3 +1,11 @@
+delete from fornecedores;
+
+delete from clientes;
+
+delete from pessoas;
+
+ALTER SEQUENCE pessoas_id_seq RESTART WITH 1;
+
 INSERT INTO pessoas (cidade_id,nome,logradouro,logradouro_numero,bairro,cep,email,data_cadastro)					
 
 VALUES 
@@ -9,14 +17,14 @@ VALUES
 INSERT INTO clientes (pessoa_id, data_nascimento, renda_mensal,documento,limite_de_credito,dia_vencimento_fiado)
 
 VALUES
-(14,null,12000,null,2000,10),
-(15,'15/08/1991',1250.00,35687454,375.00,10),
-(16,'27/11/1994',3250.00,null,975.00,15);
+(2,null,12000,null,2000,10),
+(3,'15/08/1991',1250.00,35687454,375.00,10),
+(4,'27/11/1994',3250.00,null,975.00,15);
 
 INSERT INTO fornecedores(pessoa_id,cnpj)
 
 VALUES
-(13,12345678000100),
-(14,32445677382100);
+(1,12345678000100),
+(2,32445677382100);
 
 UPDATE empresa SET db_versao = 'B0610';
