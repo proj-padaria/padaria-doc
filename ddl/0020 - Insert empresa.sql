f3e38c07fd439b63829564b6521a56c39e6e86e0
Gustavo <meiragustavo@hotmail.com>
2023-09-22 19:56:57 -0300

    Ajustado ddl's para evitar erros de id , realizados insert em empresa, cidades,planos_pagamento e planos_pagamento_dias, script ok testado e rodando serm erros

--- ddl/0020 - Insert empresa.sql
+++ ddl/0020 - Insert empresa.sql
@@ -1,18 +1,30 @@
+delete from empresa;
+
 INSERT INTO empresa (razao_social, nome_fantasia, db_versao)
 VALUES 
 ('Padaria do Seu Joaquim', 'Joaquim da esquina', 'B0011');
+
+delete from cidades;
+
+ALTER SEQUENCE cidades_id_seq RESTART WITH 1 ;
 
 INSERT INTO cidades (uf_id, nome)
 VALUES 
 (24, 'São José do Rio Preto'),
 (18, 'Petrópolis'),
 (20, 'Gramado'),
-(23, 'Balneário Camboriú'),
+(23, 'Balneário Camboriú'),  
 (16, 'Recife'),
 (5, 'Porto Seguro'),
 (15, 'Curitiba'),
 (24, 'Catanduva'),
 (12, 'Belo Horizonte');
+
+delete from planos_pagamento_dias;
+
+delete from planos_pagamento;
+
+ALTER SEQUENCE planos_pagamento_id_seq RESTART WITH 1;
 
 INSERT INTO planos_pagamento (descricao, prazo_medio)
 VALUES
@@ -21,6 +33,10 @@ VALUES
 ('30 - 60 - 90 dias', 60),
 ('45 dias', 45),
 ('15 - 30 - 45 - 60 dias', 38); -- Prazo médio = (15 + 30 + 45 + 60) / 4
+
+delete from planos_pagamento_dias;
+
+ALTER SEQUENCE planos_pagamento_dias_id_seq RESTART WITH 1;
 
 INSERT INTO planos_pagamento_dias (plano_pagamento_id,dias_do_vencimento,percentual_pagamento)
 VALUES
