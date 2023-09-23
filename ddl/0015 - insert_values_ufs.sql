1a9f73bab61b7fcd185d3aa6071e73db975b8dc1
Gustavo <meiragustavo@hotmail.com>
2023-09-22 22:10:30 -0300

    Adicionado os deletes na primeira ddl na ordem de exclusao dos valores da tabela

--- ddl/0015 - insert_values_ufs.sql
+++ ddl/0015 - insert_values_ufs.sql
@@ -1,3 +1,39 @@
+delete from planos_pagamento_dias;
+
+delete from planos_pagamento;
+
+delete from clientes;
+
+delete from pessoas;
+
+delete from cidades;
+
+delete from ufs;
+
+delete from empresa;
+
+delete from produtos;
+
+delete from departamentos;
+
+delete from telefones;
+
+delete from fornecedores;
+
+delete from receber;
+
+delete from vendas;
+
+delete from vendas_itens;
+
+delete from pagar;
+
+delete from compras;
+
+delete from compras_itens;
+
+
+
 delete from vendas_itens;
 
 delete from vendas;
@@ -12,12 +48,9 @@ delete from pagar;
 
 delete from compras;
 
-delete from pessoas;
-
 delete from cidades;
 
 delete from ufs ;
-
 
 ALTER SEQUENCE ufs_id_seq RESTART WITH 1;
 
