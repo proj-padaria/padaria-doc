72b733df6097d67aac12a508907b6273307a931e
anaju0113 <julinha.ajsp@gmail.com>
2023-09-25 20:44:04 -0300

    Antes de inserir ufs estava executando delete from nas tabelas que ainda não foram criadas, agora foi corrigido.

--- ddl/0015 - insert_values_ufs.sql
+++ ddl/0015 - insert_values_ufs.sql
@@ -1,38 +1,3 @@
-delete from planos_pagamento_dias;
-
-delete from compras_itens;
-
-delete from pagar;
-
-delete from compras;
-
-delete from planos_pagamento;
-
-delete from clientes;
-
-delete from fornecedores;
-
-delete from pessoas;
-
-delete from cidades;
-
-delete from ufs;
-
-delete from empresa;
-
-delete from produtos;
-
-delete from departamentos;
-
-delete from telefones;
-
-delete from receber;
-
-delete from vendas;
-
-delete from vendas_itens;
-
-
 ALTER SEQUENCE ufs_id_seq RESTART WITH 1;
 
 INSERT INTO ufs (sigla , nome)	
