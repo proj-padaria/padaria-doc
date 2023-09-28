26f5581bc62fe008a2ea890e8db087f3e5489680
Gustavo <meiragustavo@hotmail.com>
2023-09-28 15:36:44 -0300

    Ajustado ddl para insert simples , nao necessario function , tambem foi corrigido o erro do campo pessoa_id que nao existia anteriormente

--- ddl/0600 - Insert cliente consumidor.sql
+++ ddl/0600 - Insert cliente consumidor.sql
@@ -1,19 +1,9 @@
-CREATE OR REPLACE FUNCTION f_inserir_cliente_consumidor() 
-RETURNS VOID AS $$
-BEGIN
-    -- Inserir o cliente consumidor na tabela (Todos os exemplos encontrados fala sobre ter uma tabela clientes_consumidores)
+-- Inserir o cliente consumidor na tabela (Todos os exemplos encontrados fala sobre ter uma tabela clientes_consumidores)
     INSERT INTO pessoas (id, cidade_id, nome, logradouro, logradouro_numero, bairro, cep, data_cadastro)
 	VALUES (1, 2, 'Consumidor', 'N/D', 'S/N', 'N/D', '00000-000', CURRENT_DATE);
 	
-	INSERT INTO clientes (id, renda_mensal, limite_de_credito, dia_vencimento_fiado)
-    VALUES (1, 0.00, 0.00, 1);
+	INSERT INTO clientes (id, pessoa_id, renda_mensal, limite_de_credito, dia_vencimento_fiado)
+    VALUES (1, 1, 0.00, 0.00, 1);
     
-    -- Commit da transação
-    COMMIT;
     
-    -- Retornar uma mensagem de sucesso
-    RAISE NOTICE 'Cliente consumidor inserido com sucesso!';
-END;
-$$ LANGUAGE plpgsql;
-
 UPDATE empresa SET db_versao = 'B0632';
