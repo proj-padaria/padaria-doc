f38f7257f67682700dd15f8ede7c235a1ca59ddb
Gustavo <meiragustavo@hotmail.com>
2023-11-06 22:02:15 -0300

    Ajustado function para retorno de valor total vendido por bairro

--- ddl/0690 - function_venda_media_bairro.sql
+++ ddl/0690 - function_venda_media_bairro.sql
@@ -1,31 +1,28 @@
 CREATE OR REPLACE FUNCTION f_venda_media_bairro(data_inicial date, data_final date)
-RETURNS TABLE (produto_nome varchar(45), pessoa_bairro varchar(30)
-			   , quantidade_media_por_bairro numeric(8,3)) AS $$
+RETURNS TABLE (nome_bairro varchar(30) , valor_medio_por_bairro numeric(8,3))
+AS $$
 DECLARE
 	quantidade_dias int;
 BEGIN	
 	quantidade_dias = data_final - data_inicial;
 	
 RETURN QUERY
-	SELECT pr.nome,
-		CASE
-		WHEN pe.nome = 'Consumidor' THEN 'N/D'
-		ELSE bairro
-		END AS bairro,
- 		SUM(vi.quantidade/quantidade_dias) AS quantidade_media_por_bairro
+	SELECT
+CASE
+WHEN pe.nome = 'Consumidor' THEN 'N/D'
+ELSE bairro
+END AS bairro,
+ ROUND(SUM(v.valor_total/quantidade_dias),2)AS valor_medio_por_bairro
 	FROM pessoas pe
 		INNER JOIN clientes cl
 			ON pe.id = cl.pessoa_id
 	    INNER JOIN vendas v
 	   		ON cl.id = v.cliente_id	
-		INNER JOIN vendas_itens vi
-			ON v.id = vi.venda_id
-		INNER JOIN produtos pr
-			ON vi.produto_id = pr.id	   	
-	 	WHERE v.data BETWEEN data_inicial AND data_final
-	 	GROUP BY pe.nome,pe.bairro,pr.nome;
+		WHERE v.data BETWEEN data_inicial AND data_final
+	 	GROUP BY pe.nome,pe.bairro;
 END;
 $$ LANGUAGE plpgsql
+
 
 
 
